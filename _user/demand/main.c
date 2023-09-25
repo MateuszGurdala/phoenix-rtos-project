87e6d2a3a66b05c562e9c4a40fef56c1a2079df7
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2023-09-25 18:40:02 +0200

    Fixed data demand

--- _user/demand/main.c
+++ _user/demand/main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <phoenix/monitor.h>
 #include <sys/msg.h>
-#include <monitor/get_ondemand_data.h>
+#include <sys/threads.h>
+#include <phoenix/monitor.h>
+
+#define O_RDONLY 0x0001
 
 int main(int argc, char **argv)
 {
+	char cport;
+	int fildes, port;
+	msg_t msg = {
+		.type = mtRead,
+		.i.data = NULL,
+		.i.size = 0,
+		.o.data = NULL,
+		.o.size = 0
+	};
 
-	if (argc != 2)
-	{
+	if (argc != 2) {
+
 		printf("ERROR: ARGUMENTS\n");
 		printf("demand FILENAME.od\n");
 		return 1;
 	}
 
-//	get_ondemand_data(argv[1]);
+	fildes = sys_open("/monitor/port", O_RDONLY);
+	sys_read(fildes, &cport, 1);
+	sys_close(fildes);
 
-	return 0;
+	port = cport - '0';
+
+	strcpy(&msg.i.raw, argv[1]);
+
+	return msgSend(port, &msg);
 }
