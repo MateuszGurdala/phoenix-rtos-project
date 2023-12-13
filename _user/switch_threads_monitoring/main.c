17a0d95c78e2a5590ccb762edc5ed49d996ecba7
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2023-12-13 22:36:33 +0100

    Added dynamic thread monitoring

--- _user/switch_threads_monitoring/main.c
+++ _user/switch_threads_monitoring/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <phoenix/monitor.h>
 #include <monitor/switch_threads_monitoring.h>
 
 int main(int argc, char **argv)
 {
-	return switch_threads_monitoring();
+	if(argc != 2)
+	{
+		printf("Invalid number of arguments.\n");
+		printf("switch_threads_monitoring(unsigned pid)\n");
+		return 1;
+	}
+
+	if(!isdigit(*argv[1]))
+	{
+		printf("Invalid arguments.\n");
+		printf("switch_threads_monitoring(unsigned pid)\n");
+		return 1;
+	}
+
+	return switch_threads_monitoring(atoi(argv[1]));
 }
